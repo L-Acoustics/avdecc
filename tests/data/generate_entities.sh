230a48b3420d0d9420df5e8081917f1e0e2cc490
Christophe Calmejane <christophe.calmejane@l-acoustics.com>
2019-05-10 23:51:14 +0200

    Added more virtual entities and script to generate entities

---
+++ tests/data/generate_entities.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+function generate_entity()
+{
+	local index="$1"
+	local entityID
+	local primaryMac
+	local secondaryMac
+	local entityName
+	local serialNumber
+	local fileName
+	local outputFolder="generatedEntities"
+	
+	mkdir -p $outputFolder
+	
+	local byteResult=$(($index / 256))
+	local byteModulo=$(($index % 256))
+	
+	printf -v entityID "0x001B92FFFF00%.4X" $index
+	printf -v primaryMac "00:1B:92:00:%.2X:%.2X" $byteResult $byteModulo
+	printf -v secondaryMac "00:1B:92:01:%.2X:%.2X" $byteResult $byteModulo
+	printf -v entityName "Virtual Entity %.4X" $index
+	printf -v serialNumber "00%.4X" $index
+	printf -v fileName "$outputFolder/Entity_%.4X.json" $index
+	
+	echo "Generating Entity '$entityName' (eid=$entityID mac1=$primaryMac mac2=$secondaryMac s/n=$serialNumber)"
+	
+	sed -e "s/###ENTITY_ID###/$entityID/" "TalkerListener_template.json" > "$fileName"
+	sed -i -e "s/###PRIMARY_MAC_ADDRESS###/$primaryMac/" "$fileName"
+	sed -i -e "s/###SECONDARY_MAC_ADDRESS###/$secondaryMac/" "$fileName"
+	sed -i -e "s/###ENTITY_NAME###/$entityName/" "$fileName"
+	sed -i -e "s/###SERIAL_NUMBER###/$serialNumber/" "$fileName"
+}
+
+for i in $(seq 0 500)
+do
+	generate_entity $i
+done
