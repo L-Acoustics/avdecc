6304f346be5c91757beae7dbcaa3ee6834a5a843
Christophe Calmejane <christophe.calmejane@l-acoustics.com>
2019-11-04 17:42:09 +0100

    VS2019 required

--- scripts/utils.sh
+++ scripts/utils.sh
@@ -1,6 +1,26 @@
 #!/bin/bash
 # Utility bash functions
 
+getOutputFolder()
+{
+	local _retval="$1"
+	local basePath="$2"
+	local arch="$3"
+	local toolset="$4"
+	local config="$5"
+	local result=""
+
+	if isMac; then
+		result="${basePath}_${arch}"
+	elif isWindows; then
+		result="${basePath}_${arch}_${toolset}"
+	else
+		result="${basePath}_${arch}_${config}"
+	fi
+
+	eval $_retval="'${result}'"
+}
+
 getFileSize()
 {
 	local filePath="$1"
@@ -33,7 +53,12 @@ getOS()
 			result="mac"
 			;;
 		linux*)
-			result="linux"
+			# We have to check for WSL
+			if [[ `uname -r` == *"Microsoft"* ]]; then
+				result="win"
+			else
+				result="linux"
+			fi
 			;;
 		*)
 			echo "ERROR: Unknown OSTYPE: $OSTYPE"
