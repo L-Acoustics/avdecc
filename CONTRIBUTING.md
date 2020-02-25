0fa8466df05d7a0cbd9915371bccf544403f3b3b
Roman Savrulin <romansavrulin@gmail.com>
2020-02-26 01:27:22 +0300

    fix contributing guide and fix_files script

--- CONTRIBUTING.md
+++ CONTRIBUTING.md
@@ -11,3 +11,19 @@ Read and follow the coding style and guidelines [from this file](CODING_STYLE_GU
 - Don't forget to update any impacted CHANGELOG file(s)
 - Run the *fix_files.sh* script
 - Start a github pull request
+
+## To run `fix_files.sh` on MAC
+
+* Update `bash` from 3.x to latest (5.x)
+
+```bash
+brew install bash
+echo "/usr/local/bin/bash" | sudo tee -a /etc/shells
+chsh -s /usr/local/bin/bash
+```
+
+* Install latest `clang-format`
+
+```bash
+brew install romansavrulin/clang-format/clang-format-lambda --HEAD
+```
--- scripts/fix_files.sh
+++ scripts/fix_files.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 FIX_FILES_VERSION="1.5"
 
@@ -6,7 +6,7 @@ echo "Fix-Files version $FIX_FILES_VERSION"
 echo ""
 
 # Check bash version
-if [[ ${BASH_VERSINFO[0]} < 5 && (${BASH_VERSINFO[0]} < 4 || ${BASH_VERSINFO[1]} < 1) ]]; then
+  if [[ ${BASH_VERSINFO[0]} < 5 && (${BASH_VERSINFO[0]} == 4 && ${BASH_VERSINFO[1]} < 1) ]]; then
   echo "bash 4.1 or later required"
   exit 255
 fi
@@ -81,8 +81,9 @@ if [[ $do_clang_format -eq 1 && -f ./.clang-format ]]; then
 	if [ $? -eq 0 ]; then
 		cf_version="$(clang-format --version)"
 		regex="clang-format version 7\.0\.0 \(tags\/RELEASE_700\/final[ 0-9]*\/WithWrappingBeforeLambdaBodyPatch\)"
-		if [[ ! "$cf_version" =~ $regex ]]; then
-			echo "Incorrect clang-format: Version 7.0.0 with WrappingBeforeLambdaBody patch required (found: $cf_version)"
+		regex2="clang-format version 1[1-9]\.[ 0-9]*"
+		if [[ ! ( "$cf_version" =~ $regex) && ! ("$cf_version" =~ $regex2) ]]; then
+			echo "Incorrect clang-format: Version 7.0.0 with WrappingBeforeLambdaBody patch or Version > 10.0 required (found: $cf_version)"
 			exit 1
 		fi
 		applyFormat "*.[chi]pp"
