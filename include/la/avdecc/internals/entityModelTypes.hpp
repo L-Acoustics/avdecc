a29f2b0df0d7c55886d02ca8347583cf12b9d571
Christophe Calmejane <christophe.calmejane@l-acoustics.com>
2025-01-14 17:45:27 +0100

    Updated references from Milan 1.0 to Milan 1.2

--- include/la/avdecc/internals/entityModelTypes.hpp
+++ include/la/avdecc/internals/entityModelTypes.hpp
@@ -1535,7 +1535,7 @@ constexpr bool operator<(StreamIdentification const& lhs, StreamIdentification c
 	return (lhs.entityID.getValue() < rhs.entityID.getValue()) || (lhs.entityID == rhs.entityID && lhs.streamIndex < rhs.streamIndex);
 }
 
-/** Probing Status - Milan 1.0 Clause 6.8.6 */
+/** Probing Status - Milan 1.2 Clause 5.3.8.6 */
 enum class ProbingStatus : std::uint8_t
 {
 	Disabled = 0x00, /** The sink is not probing because it is not bound. */
