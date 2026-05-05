928240f28dc0cd80d60c8b538f7a87879eefc92d
Louis Leenart <louis.leenart@l-acoustics.com>
2026-05-05 11:46:49 +0200

    feat: add linux and macos support for assert

--- src/utils.cpp
+++ src/utils.cpp
@@ -22,7 +22,6 @@
 * @author Christophe Calmejane
 */
 
-#include "utils.hpp"
 #include "la/avdecc/utils.hpp"
 
 #if defined(_WIN32)
@@ -37,6 +36,7 @@
 #endif // __APPLE__
 #if defined(__unix__)
 #	include <pthread.h>
+#	include <signal.h>
 #endif // __unix__
 #include <iostream>
 #include <cstdio>
@@ -250,6 +250,7 @@ void LA_AVDECC_CALL_CONVENTION displayAssertDialog(char const* const file, unsig
 {
 	bool shouldBreak{ true };
 	bool shouldAbort{ true };
+	const bool debugger = isDebuggerAttached();
 	try
 	{
 		char buffer[2048];
@@ -262,6 +263,9 @@ void LA_AVDECC_CALL_CONVENTION displayAssertDialog(char const* const file, unsig
 			buffer[BufferLastCharOffset] = 0; // Contrary to std::snprintf, std::vsnprintf does not add \0 if there is not enough room
 
 			std::cerr << buffer << std::endl;
+#if defined(_WIN32)
+			OutputDebugString(buffer);
+#endif
 
 			if (offset < BufferLastCharOffset)
 			{
@@ -273,14 +277,14 @@ void LA_AVDECC_CALL_CONVENTION displayAssertDialog(char const* const file, unsig
 					"Press 'Ignore' to try to continue normal execution\n");
 			}
 		}
-		if (isDebuggerPresent())
+		if (debugger)
 		{
 			shouldBreak = true; // Always call DebugBreak if debugger is attached
 			shouldAbort = false; // Always try to continue if debugger is attached
 		}
 		else
 		{
-#ifdef _WIN32
+#if defined(_WIN32)
 			auto const value = MessageBox(nullptr, buffer, "Assert", MB_ABORTRETRYIGNORE | MB_ICONERROR);
 			shouldBreak = (value == IDRETRY);
 			shouldAbort = (value == IDABORT);
@@ -293,11 +297,15 @@ void LA_AVDECC_CALL_CONVENTION displayAssertDialog(char const* const file, unsig
 	}
 	if (shouldBreak)
 	{
-#ifdef _WIN32
+#if defined(_WIN32)
 		DebugBreak();
+#elif defined(__APPLE__)
+		__builtin_trap();
+#elif defined(__linux)
+		raise(SIGTRAP);
 #endif
 	}
-	if (shouldAbort)
+	if (shouldAbort && !debugger)
 	{
 		std::abort();
 	}
